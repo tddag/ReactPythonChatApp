c2b7c5700db55d21e41b62513422296bde5748fe
Tam Dang <danhtam0509@gmail.com>
2024-12-28 15:51:14 -0500

    finished registerUser Page UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="underline">Hello Tom</h1>
         <SignUp/>
       </div>
     </>
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,7 +1,24 @@
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div>
+      <div className="max-w-lg mx-auto p-3">
+        <h2 className="text-3xl text-center font-semibold my-7">Sign Up</h2>
+        <form className="flex flex-col gap-4">
+          <input type="text" placeholder="Name" id="name" className="bg-slate-100 p-3 rounded-lg"/>
+          <input type="email" placeholder="Email" id="email" className="bg-slate-100 p-3 rounded-lg"/>
+          <input type="password" placeholder="Password" id="password" className="bg-slate-100 p-3 rounded-lg"/>
+          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Sign Up
+          </button>
+        </form>
+
+        <div className="flex gap-4 mt-5">
+          <p>Have an account?</p>
+          <span className="text-blue-500">Sign In</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
